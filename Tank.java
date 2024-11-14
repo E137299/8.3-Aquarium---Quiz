4d887938dc6259116d5926840b042bb76732657a
David Yin <29613693+E137299@users.noreply.github.com>
2024-11-14 09:19:22 -0600

    Update Tank.java

--- Tank.java
+++ Tank.java
@@ -1,7 +1,35 @@
-import java.util.*;
-
 public class Tank{
     private ArrayList<Fish> fishies;
     private int temp;
 
+    public Tank(int t){
+        this.temp = t;
+        fishies = new ArrayList<Fish>();
+    }
+    
+    /**
+     * Returns a list of all fish in this tank. If there are
+     * no fish in this tank, returns an empty list.
+     * @return a list of all fish in the tank or an empty list
+     */
+    public ArrayList<Fish> getFish(){
+        
+    }
+
+    /**
+     * @return the temperature of this tank
+     */
+    public int temp(){
+        
+    }
+
+    /**
+     * Adds the specified fish to this tank
+     * @param fishy the fish to add
+     */
+    public void addFish(Fish fishy){
+        
+    }
+
+    // There may be variables, constructors, and methods that are not shown.
 }
