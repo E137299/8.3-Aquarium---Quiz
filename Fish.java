36ca5004a37987ddaf69ea486248581fd124a6b0
David Yin <29613693+E137299@users.noreply.github.com>
2024-11-14 09:15:35 -0600

    Update Fish.java

--- Fish.java
+++ Fish.java
@@ -1,8 +1,23 @@
-import java.util.*;
-
 public class Fish{
-    private int minTemp;
-    private int maxTemp;
-    private ArrayList<Fish> friends;
+    /**
+     * @return the minimum temperature this fish can tolerate
+     * Postcondition: minTemp() <= maxTemp()
+     */
+    public int minTemp()
+    { /* implementation not shown */ }
 
+    /**
+     * @return the maximum temperature this fish can tolerate
+     * Postcondition: maxTemp() >= minTemp()
+     */
+    public int maxTemp()
+    { /* implementation not shown */ }
+
+    /**
+     * @return true if this fish is compatible with otherFish, false otherwise
+     */
+    public boolean isCompatible(Fish otherFish)
+    { /* implementation not shown */ }
+
+    // There may be instance variables, constructors, and methods that are not shown.
 }
